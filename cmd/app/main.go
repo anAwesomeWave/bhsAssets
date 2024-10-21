93bbd1515c1e5428e81f068ccdabcc3663b22d76
Timofey Zubov <timofey.zubov.05@gmail.com>
2024-10-21 12:33:23 +0300

    Add simple http server with router

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"bhsAssets/internal/config"
 	"bhsAssets/internal/storage"
-	"errors"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
 	"log"
+	"net/http"
 )
 
 func main() {
 	// ✔️ config (storage path, http ip:port etc.)
 	// TODO: router (chi router)
-	// TODO: storage (pg crud)
-	// TODO: migrations
+	// TODO: storage (pg crud) ✔️
+	// TODO: migrations ✔️
 	// TODO: http server (net/http server)
 	// TODO: jwt
 	args := *parseFlags()
@@ -27,12 +29,29 @@ func main() {
 		log.Fatalf(err.Error())
 	}
 	log.Println("db opened")
-	id, err := db.CreateUser("opTimus", "password")
-	if err != nil {
-		if errors.Is(err, storage.ErrExists) {
-			log.Fatalf("non unique %v", err)
-		}
-		log.Fatalf("Unhandle err %v", err)
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer) // не падать при панике
+	router.Use(middleware.URLFormat) // удобно брать из урлов данные
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	serv := &http.Server{
+		Addr:         appConfig.HTTPServerCfg.Address,
+		Handler:      router,
+		ReadTimeout:  appConfig.HTTPServerCfg.Timeout,
+		WriteTimeout: appConfig.HTTPServerCfg.Timeout,
+		IdleTimeout:  appConfig.HTTPServerCfg.IdleTimeout,
 	}
-	log.Printf("Inserted, id: %d\n", id)
+
+	log.Println("Starting server...")
+	if err := serv.ListenAndServe(); err != nil {
+		log.Fatalln("failed to start server")
+	}
+
+	_ = db
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
 	github.com/jackc/pgerrcode v0.0.0-20240316143900-6e2875d9b438 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
