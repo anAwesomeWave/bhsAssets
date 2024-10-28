4b329e149b1cf4e2199fc70237918fa40e7fc541
Timofey Zubov <timofey.zubov.05@gmail.com>
2024-10-28 12:36:49 +0300

    Add graceful shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"bhsAssets/internal/config"
 	"bhsAssets/internal/storage"
+	"context"
 	"github.com/go-chi/docgen"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -35,7 +40,26 @@ func main() {
 	log.Println("Server routes")
 	docgen.PrintRoutes(router)
 	log.Println("Starting server...")
-	if err := serv.ListenAndServe(); err != nil {
-		log.Fatalln("failed to start server")
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := serv.ListenAndServe(); err != nil {
+			log.Fatalln("failed to start server")
+		}
+	}()
+
+	<-done
+
+	log.Println("stopping server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := serv.Shutdown(ctx); err != nil {
+
+		log.Printf("failed to stop server: %v\n", err)
+		return
 	}
+	log.Println("server stopped")
 }
